dd355e90f08fb0360cc25676b21f0b2344bd17e8
Edgar Montenegro <edgarmontenegro321@gmail.com>
2023-11-16 19:49:07 -0300

    ⚙️ Add develop branch

--- src/Views/Layout.jsx
+++ src/Views/Layout.jsx
@@ -1,6 +1,6 @@
 const Layout = () => {
     return (
-        <h1>Here goes the components like header, about me and footer!</h1>
+        <h1>Layout view! Here goes the components like header, about me and footer!</h1>
     )
 }
 export default Layout
