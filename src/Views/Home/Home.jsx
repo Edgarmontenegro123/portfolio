ecfab5ff8d1e3738c26a885faaa78bf7607d4dfd
Edgar Montenegro <edgarmontenegro321@gmail.com>
2023-11-26 06:17:07 -0300

    🏠 Add Home view!

--- src/Views/Home/Home.jsx
+++ src/Views/Home/Home.jsx
@@ -1,19 +1,67 @@
-import AnimatedBackground from "../../Components/AnimatedBackground/AnimatedBackground.jsx";
-import Footer from "../../Components/Footer/Footer.jsx";
-import './Home.scss'
+import { useEffect, useState } from 'react';
+import AnimatedBackground from '../../Components/AnimatedBackground/AnimatedBackground.jsx';
+import Footer from '../../Components/Footer/Footer.jsx';
+import { Link } from 'react-router-dom';
+import './Home.scss';
 
 const Home = () => {
-    return(
+    const [text, setText] = useState('');
+    const fullText = `
+    Soy un apasionado Desarrollador Web con un trasfondo único. Comencé mi carrera en diseño de
+    indumentaria y pasé una década en gastronomía antes de sumergirme en el emocionante mundo del
+    desarrollo web en busca de crecimiento personal y profesional.
+
+    En los últimos años, he acumulado experiencia valiosa trabajando en empresas tecnológicas
+    líderes, abordando desafíos en JavaScript, GO, Java y más.
+
+    Mi enfoque proactivo y amor por el aprendizaje continuo me impulsa a crecer constantemente.
+    Mi experiencia en gastronomía fortaleció mis habilidades de trabajo en equipo y gestión del
+    tiempo, mientras que el diseño fomentó la creatividad y atención al detalle.
+
+    Busco un equipo innovador que valore la mejora continua y ofrezca oportunidades emocionantes
+    para combinar mis habilidades y seguir creciendo, a la vez que contribuyo con mi experiencia y
+    entusiasmo para alcanzar los objetivos de la empresa.
+  `;
+
+    useEffect(() => {
+        let currentIndex = 0;
+        const intervalId = setInterval(() => {
+            setText(fullText.substring(0, currentIndex));
+            currentIndex ++;
+
+            if (currentIndex > fullText.length) {
+                clearInterval(intervalId);
+            }
+        }, 30);
+
+        return () => clearInterval(intervalId);
+    }, [fullText]);
+
+    return (
         <>
             <AnimatedBackground>
-                <hr className='divSkills__divider'/>
+                <hr className='divSkills__divider' />
                 <div className='divHome'>
-                    <h1>Vista Home!</h1>
-                    <p>lorem ipsum</p>
+                    <div className='divHome__card'>
+                        <p className='divHome__animated-text'>{text}</p>
+                    </div>
+                    <br />
+                    <Link to='/contact' className='divHome__link'>
+                        <b>Conéctate conmigo!</b>
+                    </Link>
+                    <br />
+                    <Link
+                        to='https://www.canva.com/design/DAElFbGgReo/w5KZpX2OLMFUX0hXbxV56w/view?utm_content=DAElFbGgReo&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink'
+                        target='_blank'
+                        rel='noopener noreferrer'
+                    >
+                        <button className='divHome__button'>Curriculum PDF</button>
+                    </Link>
                 </div>
-                <Footer/>
+                <Footer />
             </AnimatedBackground>
         </>
-    )
-}
-export default Home
+    );
+};
+
+export default Home;
