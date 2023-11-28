097e0bd5a34ee2f4952232ae1e123c01921a0d7d
Edgar Montenegro <edgarmontenegro321@gmail.com>
2023-11-28 00:19:03 -0300

    🎛️ Add animation to Contact icons view!

--- src/Views/Contact/Contact.jsx
+++ src/Views/Contact/Contact.jsx
@@ -14,8 +14,8 @@ const Contact = () => {
                         <Link to={Constants.whatsapp}
                               target='_blank' rel='noreferrer'
                               className='divContact__link'>
-                                <span>
-                                    <i className='fa-brands fa-whatsapp'></i>
+                                <span className='fadeInUp' style={{animationDelay: `${0.1}s`}}>
+                                    <i className='fa-brands fa-whatsapp fa-beat'  style={{ '--fa-animation-duration': '2s' }}></i>
                                     <span className='divContact__span'> Teléfono: </span>{Constants.phone}
                                 </span>
                         </Link>
@@ -23,8 +23,8 @@ const Contact = () => {
                         <Link to={Constants.mailRedirect}
                               target='_blank' rel='noreferrer'
                               className='divContact__link'>
-                            <span>
-                                <i className='fa-regular fa-envelope' />
+                            <span className='fadeInUp' style={{animationDelay: `${0.3}s`}}>
+                                <i className='fa-regular fa-envelope fa-beat'  style={{ '--fa-animation-duration': '2s' }} />
                                 <span className='divContact__span'> Correo Electrónico: </span>
                                 {Constants.email}
                             </span>
@@ -33,8 +33,8 @@ const Contact = () => {
                         <Link to={Constants.linkedIn}
                               target='_blank' rel='noreferrer'
                               className='divContact__link'>
-                            <span>
-                                <i className='fa-brands fa-linkedin'></i>
+                            <span className='fadeInUp' style={{animationDelay: `${0.5}s`}}>
+                                <i className='fa-brands fa-linkedin fa-beat'  style={{ '--fa-animation-duration': '2s' }}></i>
                                 <span className='divContact__span'> LinkedIn: </span>{Constants.perfilLinkedIn}
                             </span>
                         </Link>
@@ -42,8 +42,8 @@ const Contact = () => {
                         <Link to={Constants.github}
                               target='_blank' rel='noreferrer'
                               className='divContact__link'>
-                            <span>
-                                <i className='fa-brands fa-github'></i>
+                            <span className='fadeInUp' style={{animationDelay: `${0.7}s`}}>
+                                <i className='fa-brands fa-github fa-beat'  style={{ '--fa-animation-duration': '2s' }}></i>
                                 <span className='divContact__span'> Github: </span>{Constants.perfilGithub}
                             </span>
                         </Link>
@@ -51,8 +51,8 @@ const Contact = () => {
                         <Link to={Constants.twitter}
                               target='_blank' rel='noreferrer'
                               className='divContact__link'>
-                            <span>
-                                <i className='fa-brands fa-x-twitter'></i>
+                            <span className='fadeInUp' style={{animationDelay: `${0.9}s`}}>
+                                <i className='fa-brands fa-x-twitter fa-beat'  style={{ '--fa-animation-duration': '2s' }}></i>
                                 <span className='divContact__span'> X: </span>{Constants.perfilTwitter}
                             </span>
                         </Link>
@@ -60,8 +60,8 @@ const Contact = () => {
                         <Link to={Constants.curriculumPDF}
                               target='_blank' rel='noreferrer'
                               className='divContact__link'>
-                            <span>
-                                <i className="fa-solid fa-file-pdf"></i>
+                            <span className='fadeInUp' style={{animationDelay: `${1.1}s`}}>
+                                <i className='fa-solid fa-file-pdf fa-beat'  style={{ '--fa-animation-duration': '2s' }}></i>
                                 <span className='divContact__span'>Mi Curriculum en PDF</span>
                             </span>
                         </Link>
@@ -69,8 +69,8 @@ const Contact = () => {
                         <Link to={Constants.home}
                               target='_blank' rel='noreferrer'
                               className='divContact__link'>
-                            <span>
-                                <i className='fa-solid fa-globe'></i>
+                            <span className='fadeInUp' style={{animationDelay: `${1.3}s`}}>
+                                <i className='fa-solid fa-globe fa-beat'  style={{ '--fa-animation-duration': '2s' }}></i>
                                 <span className='divContact__span'>Lugar de Residencia: </span>Buenos Aires, Argentina
                             </span>
                         </Link>
