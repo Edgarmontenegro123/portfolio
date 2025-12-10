2ac27852142bc2ce4630f7130f788d27c9702aa3
Edgar <edgarmontenegro321@gmail.com>
2025-12-10 12:10:35 -0300

    😎 Add Task Mate Evolution project

--- src/Constants/ProjectsFiles.jsx
+++ src/Constants/ProjectsFiles.jsx
@@ -1,8 +1,10 @@
+import LogoTaskMateEvolution from '../assets/Images/taskMateEvolutionLogo.png'
 import LogoQuizUp from '../assets/Images/quizUpLogo.png'
 import LogoClimApp from '../assets/Images/sunnyCloud.png'
 import LogoHealthier from '../assets/Images/healthier.png'
 import LogoYoutube from '../assets/Images/youtubeLogo.png'
 import LogoTaskMate from '../assets/Images/tasksLogo.png'
+import GifTaskMateEvolution from '../assets/Images/gifTaskMateEvolution.gif'
 import GifQuizUp from '../assets/Images/gifQuizUp.gif'
 import GifTaskMate from '../assets/Images/gifTaskMate.gif'
 import GifClimApp from '../assets/Images/gifClimApp.gif'
@@ -10,6 +12,18 @@ import GifHealthier from '../assets/Images/gifHealthier.gif'
 import GifYoutubeChallenge from '../assets/Images/gifYoutubeChallenge.gif'
 
 const MockUp = [{
+    id: 0,
+    avatar: LogoTaskMateEvolution,
+    title: 'Task Mate Evolution!',
+    date: 'Octubre 2025',
+    img: GifTaskMateEvolution,
+    resume: 'Task Mate Evolution es un gestor de tareas y notas móvil que combina texto y voz en una interfaz moderna y fluida.',
+    video: 'https://vimeo.com/manage/videos/1135584450',
+    description: 'Task Mate Evolution es una aplicación móvil desarrollada con React Native (Expo) y TypeScript, diseñada como un gestor inteligente de tareas y notas, combinando texto y grabaciones de voz dentro de una interfaz moderna, minimalista y fluida. Próximamente en Google Play y AppStore',
+    tech: 'TypeScript, React Native, Expo SDH 54',
+    deploy: 'https://task-mate-by-montenegro.vercel.app/',
+    gitHub: 'https://github.com/Edgarmontenegro123/task-mate-evolution'},
+    {
     id: 1,
     avatar: LogoQuizUp,
     title: 'QuizUp!',
@@ -22,17 +36,17 @@ const MockUp = [{
     deploy: 'https://quizup-by-montenegro.vercel.app/',
     gitHub: 'https://github.com/Edgarmontenegro123/quizup'},
     {
-    id: 2,
-    avatar: LogoTaskMate,
-    title: 'Task Mate',
-    date: 'Septiembre 2024',
-    img: GifTaskMate,
-    resume: 'Aplicación basada en el famoso proyecto To-Do List, potenciada con diferentes funcionalidades.',
-    video: 'https://vimeo.com/manage/videos/1010457455',
-    description: 'Esta es una App con una interfaz amigable, que permite agregar notas o tareas, modificarlas, separarlas por colores, organizarlas con un simple arrastre, eliminar las tareas completadas y guardarlas en tu dispositivo entre otras funcionalidades.',
-    tech: 'Typescript, React, NextJs, Tailwind, HTML5, CSS3',
-    deploy: 'https://task-mate-by-montenegro.vercel.app/',
-    gitHub: 'https://github.com/Edgarmontenegro123/task-mate'},
+    id: 4,
+    avatar: LogoHealthier,
+    title: 'Healthier',
+    date: 'Agosto 2021',
+    img: GifHealthier,
+    resume: 'E-Commerce realizado de manera grupal como proyecto final para Soy Henry Bootcamp.',
+    video: 'https://vimeo.com/manage/videos/586633960',
+    description: 'Con una interfaz muy intuitiva, que permite comprar productos saludables, ordenarlos por precio, por categoría, incluirlos en una wishlist  o llevarlos a un carrito de compra donde se permite terminar el proceso de compra mediante una pasarela de pagos.',
+    tech: 'React, Redux, Material UI, NodeJS, PostgreSQL',
+    deploy: 'https://Healthier.vercel.app',
+    gitHub: 'https://github.com/Healthier-Group/Healthier'},
     {
     id: 3,
     avatar: LogoClimApp,
@@ -46,17 +60,17 @@ const MockUp = [{
     deploy: 'https://climappmontenegro.vercel.app',
     gitHub: 'https://github.com/Edgarmontenegro123/ClimApp'},
     {
-    id: 4,
-    avatar: LogoHealthier,
-    title: 'Healthier',
-    date: 'Agosto 2021',
-    img: GifHealthier,
-    resume: 'E-Commerce realizado de manera grupal como proyecto final para Soy Henry Bootcamp.',
-    video: 'https://vimeo.com/manage/videos/586633960',
-    description: 'Con una interfaz muy intuitiva, que permite comprar productos saludables, ordenarlos por precio, por categoría, incluirlos en una wishlist  o llevarlos a un carrito de compra donde se permite terminar el proceso de compra mediante una pasarela de pagos.',
-    tech: 'React, Redux, Material UI, NodeJS, PostgreSQL',
-    deploy: 'https://Healthier.vercel.app',
-    gitHub: 'https://github.com/Healthier-Group/Healthier'},
+    id: 2,
+    avatar: LogoTaskMate,
+    title: 'Task Mate',
+    date: 'Septiembre 2024',
+    img: GifTaskMate,
+    resume: 'Aplicación basada en el famoso proyecto To-Do List, potenciada con diferentes funcionalidades.',
+    video: 'https://vimeo.com/manage/videos/1010457455',
+    description: 'Esta es una App con una interfaz amigable, que permite agregar notas o tareas, modificarlas, separarlas por colores, organizarlas con un simple arrastre, eliminar las tareas completadas y guardarlas en tu dispositivo entre otras funcionalidades.',
+    tech: 'Typescript, React, NextJs, Tailwind, HTML5, CSS3',
+    deploy: 'https://task-mate-by-montenegro.vercel.app/',
+    gitHub: 'https://github.com/Edgarmontenegro123/task-mate'},
     {
     id: 5,
     avatar: LogoYoutube,
